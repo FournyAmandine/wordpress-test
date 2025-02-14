9ddc31c6177c8d7bef758f296bfb779af7470540
FournyAmandine <amandine.fourny@student.hepl.be>
2025-02-14 09:09:19 +0100

    removed gutenberg

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -1,1 +1,21 @@
 <?php
+
+//Gutenberg est le nouvel éditeur de contenu propre à wordpress, il ne nous intéresse pas pour l'utilisation du thème que nous allons créer
+
+// Disable Gutenberg on the back end.
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg for widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
+// Disable default front-end styles. 
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the front end.
+    wp_dequeue_style( 'wp-block-library' );
+    // Remove Gutenberg theme.
+    wp_dequeue_style( 'wp-block-library-theme' );
+    // Remove inline global CSS on the front end.
+    wp_dequeue_style( 'global-styles' );
+}, 20 );
+
+//queue : mise en file. Prépare l'objet pour charger les éléments
+//enqueue : enlève de la file d'attente
