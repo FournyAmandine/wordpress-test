5ea9f0e87bd1e78f7a688b74cf8641b56c85a1a8
FournyAmandine <amandine.fourny@student.hepl.be>
2025-03-21 09:35:48 +0100

    Added Contact Form 7 using and do shortcode

--- wp-content/themes/dw/template-contact.php
+++ wp-content/themes/dw/template-contact.php
@@ -4,10 +4,14 @@
     <aside>
         <h2>Contactez-moi!</h2>
     </aside>
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-    <div><?= get_the_content(); ?></div>
-        <?php endwhile; else: ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <section class="contact">
+        <div class="contact__left"><?= get_the_content(); ?></div>
+        <div class="contact__right">
+            <?= do_shortcode('[contact-form-7 id="ec509ed" title="Formulaire page contact"]')?>
+        </div>
+    </section>
+<?php endwhile; else: ?>
     <p>La page est vide</p>
 <?php endif; ?>
 <?php get_footer(); ?>
